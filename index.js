ec6bece0cd0d1405d4b43256b35b7a7ac4805467
Juan José Peña <jjpenad@uniandes.edu.co>
2021-03-01 18:44:44 -0500

    ClientSide Rendering

--- index.js
+++ index.js
@@ -14,9 +14,42 @@ function setTables(callback) {
 			fs.readFile('index.html', (err, data) => {
 				if (err) throw err;
 
+				// Total of records in the table Events
+				let total = json.length;
+
+				// Number of records whose "squirrel" value is true
+				let nTrue = 0;
+
+				// Map to store the events' ocurrences
+				let map = new Map();
+
+				// Function to add an event count to the map
+				function addToMap(event, squirrel) {
+					let found = map.has(event);
+					if (found) {
+						let ev = map.get(event);
+						if (squirrel) {
+							ev['appTrue'] += 1;
+						}
+						else {
+							ev['appFalse'] += 1;
+						}
+					}
+					else {
+						if (squirrel) {
+							map.set(event, { appTrue: 1, appFalse: 0 });
+						}
+						else {
+							map.set(event, { appTrue: 0, appFalse: 1 });
+						}
+					}
+				}
+
+				// Parsing HTML
 				const root = parse(data.toString());
 				const body = root.querySelector('body');
 
+				//------------------------------------------------------------Events Table------------------------------------------------------------
 				// Reference to table of Events
 				const tEvents = body.querySelector('#tEvents');
 
@@ -35,14 +68,6 @@ function setTables(callback) {
 				tSubtitles.appendChild(parse('<th scope="col">#</th>'));
 				keys.forEach((key) => tSubtitles.appendChild(parse('<th scope="col">' + key + '</th>')));
 
-				/*
-				let keysAppend = [];
-
-				keys.forEach((key) => keysAppend.push(parse('<th scope="col">' + key + '</th>')));
-
-				tSubtitles.set_content(keysAppend);
-				*/
-
 				let i = 1;
 				json.forEach((item) => {
 					let row = '<th>' + i + '</th>';
@@ -55,11 +80,14 @@ function setTables(callback) {
 						else {
 							if (typeof obj == 'boolean' && obj == true) {
 								squirrel = true;
+								nTrue++;
 							}
-
 							row += '<th>' + obj + '</th>';
 						}
 					});
+
+					// Add events to map
+					item['events'].forEach((key) => addToMap(key, squirrel));
 
 					if (squirrel) {
 						tbody.appendChild(parse('<tr class="table-danger">' + row + '</tr>'));
@@ -69,7 +97,71 @@ function setTables(callback) {
 					}
 					i++;
 				});
+				//------------------------------------------------------------------------------------------------------------------------------------
 
+				//------------------------------------------------------------Correlation Table-------------------------------------------------------
+				// Reference to table of Correlation
+				const tCorrelation = body.querySelector('#tCorrelation');
+
+				// Head and body of the table
+				const theadC = tCorrelation.querySelector('thead');
+				const tbodyC = tCorrelation.querySelector('tbody');
+
+				const tTitleC = theadC.querySelector('#tTitleC');
+				const tSubtitlesC = theadC.querySelector('#tSubtitlesC');
+
+				tTitleC.appendChild(parse('<h1>Correlation of Events</h1>'));
+
+				tSubtitlesC.appendChild(parse('<th scope="col">#</th>'));
+				tSubtitlesC.appendChild(parse('<th scope="col">Event</th>'));
+				tSubtitlesC.appendChild(parse('<th scope="col">Correlation</th>'));
+
+				let eventCorrelation = [];
+				for (const [ key, value ] of map) {
+					let fN = value['appFalse'];
+					let tP = value['appTrue'];
+					let fP = nTrue - value['appTrue'];
+					let tN = total - fN - tP - fP;
+
+					let corr = (tP * tN - fP * fN) / Math.sqrt((tP + fP) * (tP + fN) * (tN + fP) * (tN + fN));
+
+					eventCorrelation.push({ event: key, Correlation: corr });
+					//console.log(`${key}: ${value['appTrue']}`);
+				}
+
+				eventCorrelation.sort((firstEl, secondEl) => {
+					let corFirst = firstEl['Correlation'];
+					let corSecond = secondEl['Correlation'];
+					if (corFirst > corSecond) {
+						return -1;
+					}
+					if (corFirst < corSecond) {
+						return 1;
+					}
+					return 0;
+				});
+
+				let num = 1;
+				eventCorrelation.forEach((elem) => {
+					tbodyC.appendChild(
+						parse(
+							'<tr>' +
+								'<th>' +
+								num +
+								'</th>' +
+								'<th>' +
+								elem['event'] +
+								'</th>' +
+								'<th>' +
+								elem['Correlation'] +
+								'</th>' +
+								'</tr>'
+						)
+					);
+					num++;
+				});
+
+				//------------------------------------------------------------------------------------------------------------------------------------
 				callback(root.toString());
 			});
 		})
